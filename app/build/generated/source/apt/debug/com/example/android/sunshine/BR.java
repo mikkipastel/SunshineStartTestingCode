b61718b70f219aa84485a115f03620ff45e020ad
Monthira Chayabanjonglerd <monthira@fungjai.com>
2017-10-12 11:23:45 +0700

    init

--- app/build/generated/source/apt/debug/com/example/android/sunshine/BR.java
+++
@@ -1,6 +0,0 @@
-package com.example.android.sunshine;
-
-@javax.annotation.Generated("Android Data Binding")
-public class BR {
-        public static final int _all = 0;
-}
--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="b9cec3c4-4c24-4b2b-b39a-b446802d4a16")
-public class DataBindingInfo {}
